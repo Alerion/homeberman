907f85b956706bee20806e3c0cdc10966a24fb75
chem <chem@sonamini>
2011-07-09 19:52:06 +0400

    add dummy game model

--- src/main/models.py
+++ src/main/models.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from accounts.models import User
+class Game(models.Model):
+    name = models.CharField(max_length=140, null=True, blank=True)
+    is_waiting = models.BooleanField(u"Waiting users", default=True)
 
-
+    players = models.ManyToManyField(User, blank=True)
+    
