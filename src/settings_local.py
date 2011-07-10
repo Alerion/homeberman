26d9354f3fbf9bea7e5329a0bb6fbcbdda4025c8
chem <chem@sonamini>
2011-07-10 09:10:05 +0400

    remove local settings from git repo

--- src/settings_local.py
+++
@@ -1,6 +0,0 @@
-from settings import INSTALLED_APPS, rel
-
-# python -m smtpd -n -c DebuggingServer localhost:1025
-EMAIL_HOST_USER = 'example@localhost'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
